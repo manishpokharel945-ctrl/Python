746f80c450ed30dd73c28a809e1bf968bf02aa5c
Manish Pokharel <manishpokharel945@gmail.com>
2026-05-18 21:54:51 +0545

    Add files via upload

---
+++ loop day 4.py
@@ -0,0 +1,15 @@
+#i = 1
+#while i <= 100:
+    #print(i)
+    #i += 1
+
+#i = 100
+#while i >= 1:
+    #print(i)
+    #i -= 1
+
+num = int(input("enter a number"))
+i=1
+while i <= 10:
+    print(num*i)
+    i += 1
