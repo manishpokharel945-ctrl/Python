d49412792204941855f0ac884b4bfc7f5bec3d45
Manish Pokharel <manishpokharel945@gmail.com>
2026-05-20 20:27:36 +0545

    Add files via upload

---
+++ list and tuple.py
@@ -0,0 +1,17 @@
+#list
+fruits = ["apple", "banana", "mango"]
+
+print(fruits)
+
+fruits.append("orange")   # add new item
+print(fruits)
+
+fruits[1] = "grapes"      # change banana to grapes
+print(fruits)
+
+#tuple
+colors = ("red", "blue", "green")
+
+print(colors)
+
+print(colors[0])   # access first item
