933e9bb63b3dabd6e2c6fbca94d066e2201cf72e
Manish Pokharel <manishpokharel945@gmail.com>
2026-05-23 21:59:02 +0545

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,27 @@
+print("Calculator")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+operator = input("Enter operator (+, -, *, /): ")
+
+if operator == "+":
+    result = num1 + num2
+    print("Result:", result)
+
+elif operator == "-":
+    result = num1 - num2
+    print("Result:", result)
+
+elif operator == "*":
+    result = num1 * num2
+    print("Result:", result)
+
+elif operator == "/":
+    if num2 != 0:
+        result = num1 / num2
+        print("Result:", result)
+    else:
+        print("Error: Cannot divide by zero")
+
+else:
+    print("Invalid operator")
